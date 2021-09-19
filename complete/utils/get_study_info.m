ed95fa5d7970b78592c90b9087698c7b970b019c
= <=>
2021-09-19 16:15:18 +0200

    hardcoded info

---
+++ complete/utils/get_study_info.m
@@ -0,0 +1,27 @@
+function [s_info] = get_study_info(study)
+%function [s_info] = get_study_info(study)
+% study is either 'preproc' or 'rose'
+%
+%This function returns information about the studies from which the 
+%preprocessing and first/secondlevel data comes from
+%We use it because hardcoding is a bad idea and if we have to do it, then
+%it's best to have it only in one place. If we change the TR, we only
+%change it in ONE place, and the rest still runs
+
+s_info = [];
+
+switch study
+    case 'rose'
+        TR = 2.6;
+        n_slices = 32;
+    case 'preproc'
+        TR = 2.87;
+        n_slices = 48;
+    otherwise
+        error('Unknown option. Try ''preproc'' or ''rose''.');
+end
+
+s_info.TR = TR;
+s_info.n_slices = n_slices;
+
+end
